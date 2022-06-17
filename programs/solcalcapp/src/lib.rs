a42ee5a036cc6d32fbda4f036e717fd88937a6a1
tahlil <bsse0803@iit.du.ac.bd>
2022-06-17 21:27:13 +0600

    Add create function

--- programs/solcalcapp/src/lib.rs
+++ programs/solcalcapp/src/lib.rs
@@ -5,11 +5,12 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solcalcapp {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.gteeting = init_message;
         Ok(())
     }
+    
+   
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
