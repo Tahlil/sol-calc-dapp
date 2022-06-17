7317d103cc66e1bc125de5f53d553142c4584a75
tahlil <bsse0803@iit.du.ac.bd>
2022-06-17 22:36:10 +0600

    Pass test

--- programs/solcalcapp/src/lib.rs
+++ programs/solcalcapp/src/lib.rs
@@ -5,7 +5,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod solcalcdapp {
     use super::*;
-    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+    pub fn create(ctx: Context<Create>, init_message: String) -> Result<()> {
         let calculator = &mut ctx.accounts.calculator;
         calculator.gteeting = init_message;
         Ok(())
@@ -15,9 +15,9 @@ pub mod solcalcdapp {
 #[derive(Accounts)]
 pub struct Create<'info> {
     #[account(init, payer=user, space=264)]
-    pub calculator: Account<'info, Calculator>
+    pub calculator: Account<'info, Calculator>,
     #[account(mut)]
-    pub user: Signer<'info>
+    pub user: Signer<'info>,
     pub system_program: Program<'info, System>
 }
 
